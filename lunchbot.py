ed3a20e2b33444091352f4d48b5edc0eee4485d6
R. Udall <rudall@caltech.edu>
2022-02-17 00:02:35 -0800

    Added functionality

--- lunchbot.py
+++ lunchbot.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import discord 
 from discord.ext import commands
 from datetime import datetime
 import pytz
@@ -12,21 +13,39 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
+def check_lunch_time():
+    pc_time_rn = datetime.now(tz_caltech)
+    hour = pc_time_rn.hour
+    minute = pc_time_rn.minute
+    if 60 * hour + minute > 660 and 60 * hour + minute < 930:
+        return True
+    else:
+        return False
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-@bot.command(name='lunch', help="Is it lunch yet?")
-async def check_lunch(ctx):
+@bot.command(name='lunchtime', help="Is it lunch yet?")
+async def respond_lunchtime(ctx):
     if ctx.message.channel.name == 'lunch':
-        pc_time_rn = datetime.now(tz_caltech)
-        hour = pc_time_rn.hour
-        minute = pc_time_rn.minute
-        if 60 * hour + minute > 660 and 60 * hour + minute < 930:
+        if check_lunch_time():
             is_it_lunch = "It's lunchtime!"
         else:
             is_it_lunch = "It's not time for lunch yet :("
         await ctx.send(is_it_lunch)
+    else:
+        return
+
+@bot.command(name="clowns_assemble", help="Summon the clowns for lunch")
+async def summon_clowns(ctx):
+    if ctx.message.channel.name == 'lunch':
+        if check_lunch_time():
+            clown = discord.utils.get(ctx.guild.roles, name="🤡")
+            summons_message = "{} you are summoned for lunch, clowns.".format(clown.mention)
+        else:
+            summons_message = "You can't summon the clowns, it's not lunch time yet!"
+        await ctx.send(summons_message)
     else:
         return
 
